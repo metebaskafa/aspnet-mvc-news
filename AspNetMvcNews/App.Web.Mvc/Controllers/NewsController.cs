20dd16ce72e6a6cdb318b8b93efee77289534aa9
BurakAlanyali <16alanyali@windowslive.com>
2023-07-03 22:38:02 +0300

    task1-2-3-4-5-6

---
+++ AspNetMvcNews/App.Web.Mvc/Controllers/NewsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Search(string q, int page)
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
