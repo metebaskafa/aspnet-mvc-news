ec0a50172267d220bb83035662fe651638379489
DeLoreaann <ozdemirr.mail@gmail.com>
2023-07-20 16:40:04 +0300

    Hatalar Giderildi

---
+++ AspNetMvcNews/App.Web.Mvc/Models/TodayNewsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using App.Data.Entity;
+
+namespace App.Web.Mvc.Models
+{
+    public class TodayNewsViewModel
+    {
+        public List<News> News { get; set; }
+        public List<int> CommentCount { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
